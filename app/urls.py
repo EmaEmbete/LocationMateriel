4cf13f6963d8c1b0e841e3262d9ce30884176107
EmaEmbete <embetemmelelwa@gmail.com>
2023-01-29 00:52:08 +0200

    ajout des urls pour clients

--- app/urls.py
+++ app/urls.py
@@ -9,6 +9,13 @@ urlpatterns = [
     path('',home.index, name='home'),
 
 
+    path('clients/',clients.index, name='clients_index'),
+    path('clients/create',clients.create, name='clients_create'),
+    path('clients/store', clients.store, name='clients_store'),
+    path('clients/edit/<int:id>', clients.edit, name='clients_edit'),
+    path('clients/delete/<int:id>',clients.delete, name='clients_delete'),
+
+
 
 
 ]
